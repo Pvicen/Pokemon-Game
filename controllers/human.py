c83f54c8b3d4bf275985fc08ad540b0ecd144430
Pvicen <126504149+Pvicen@users.noreply.github.com>
2025-08-19 03:12:47 +0000

    subo archivo modificado con nuevas funciones

--- controllers/human.py
+++ controllers/human.py
@@ -1,5 +1,6 @@
 from ..models import Pokemon, EvolvedPokemon
-from ..utils import load_items
+from ..utils import load_items, load_pokemons_json
+
 
 class HumanController():
     
@@ -9,4 +10,30 @@ class HumanController():
         
         print("Avaliavle items for use")
         
+    def SelecPokemon(actor):
+        
+        initial_pokemons = load_pokemons_json()
+        option1 = initial_pokemons[2]
+        option2 = initial_pokemons[4]
+        option3 = initial_pokemons[6]
+        print("Hello player, Now you need choose your first pokemon thats your options")
+        
+
+    def ChooseAttack(actor):
+
+        all_attacks = list(actor.normal_attakcs) + list(actor.special_attakcs)
+
+        for atk in all_attacks:
+            if isinstance(atk, dict):
+                continue
+
+        if not atk:
+            print("Attacks not avaliable")
+            return
+
+        if atk["normal"] == "normal":
+            print(f"name: {atk["name"]} type: {atk["type"]} damage: {atk["damage"]}") 
+        else:
+            print(f"name: {atk["name"]} type: {atk["type"]} damage: {atk["damage"]}")
+
         
