91bbab559a5642ea11b214461131fb00345cf67f
Pvicen <morales07vicente@gmail.com>
2025-09-05 15:48:00 -0400

    añado archivo de setup_game para instanciar y crear todo

---
+++ setup_game.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+from typing import Iterable, List, Dict, Optional
+
+from .models import Pokemon
+from .trainers import Trainer
+from .inventory import Inventory
+from .utils import load_attacks_json
